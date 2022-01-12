55138c2b7ea3a15fc51cba007790191a91fb2ace
Asim <mohammadasim78800@gmail.com>
2022-01-12 20:56:24 +0530

    Second Commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,14 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-router.get('/', (req,res) => res.send('Auth Route'));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Sevrer Error");
+  }
+});
 
 module.exports = router;
