7aada9a6d470e538ae6c466a9a1982994cfea610
Madhusudan23082000 <73375985+Madhusudan23082000@users.noreply.github.com>
2022-01-13 13:22:54 +0530

    4.4 done chnages in profile.js

--- config/db.js
+++ config/db.js
@@ -5,6 +5,8 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
     try{
         await mongoose.connect(db);
+        
+       
 
         console.log('MongoDB Connected...');
     }
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -104,4 +104,28 @@ router.post(
   }
 );
 
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar'])
+    if(!profile) return res.status(400).json({ msg: 'profile not found'});
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'profile not found'});
+    }
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
