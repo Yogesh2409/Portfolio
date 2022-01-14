64cee0f912828541feeea2ffaa1f267ebc4c07f8
Asim <mohammadasim78800@gmail.com>
2022-01-14 11:39:17 +0530

    updated post.js in routes/api

--- models/Post.js
+++ models/Post.js
@@ -1,58 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 const PostSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
     },
-    text:{
-        type:String,
-        required:true
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
     },
-    name:{
-        type:String,
-       
-    },
-    avatar:{
-        type:String,
-        
-    },
-    likes:[
-        {
-            user:{
-                type:mongoose.SchemaTypes.ObjectId,
-                ref:'users'
-            }
-        }
-    ],
-    comments:[
-        {
-            user:{
-                type:mongoose.SchemaTypes.ObjectId,
-                ref:'users'
-            },
-            text:{
-              type:String,
-              required:true  
-            },
-            name:{
-                type:String,
-               
-            },
-            avatar:{
-                type:String,
-                
-            },
-            date:{
-                type:Date,
-                default:Date.now
-            }
-        }
-    ],
-    date:{
-        type:Date,
-        default:Date.now
-    }
-    
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
-modules.exports=Post= mongoose.model('post',PostSchema);
+module.exports = Post = mongoose.model("post", PostSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const request = require('request');
-const config = require('config');
+const request = require("request");
+const config = require("config");
 // const { is } = require("express/lib/request");
 const router = express.Router();
 const auth = require("../../middleware/auth");
@@ -284,29 +284,31 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
 //@desc    Get user repos from github
 //@access  Public
 
-router.get('/github/:username', (req,res) => {
+router.get("/github/:username", (req, res) => {
   try {
     const options = {
-      uri : `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
-      method : 'GET',
-      headers : { 'user-agent' : 'node.js' }
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
     };
 
     request(options, (error, response, body) => {
-      if(error){
+      if (error) {
         console.error(error);
       }
-      if(response.statusCode != 200){
-        return res.status(404).json({ msg : 'No GitHub profile found' });
+      if (response.statusCode != 200) {
+        return res.status(404).json({ msg: "No GitHub profile found" });
       }
-      res.json(JSON.parse(body)); 
-      
+      res.json(JSON.parse(body));
     });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server error');
+    res.status(500).send("Server error");
   }
 });
-
 
 module.exports = router;
