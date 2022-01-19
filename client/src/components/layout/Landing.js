92edfa858147a65578daff48bb48cb873a0895a8
Yogesh <yogeshj591@gmail.com>
2022-01-19 13:41:16 +0530

    React Router Setup Done

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
-export const Landing = () => {
+import { Link } from 'react-router-dom';
+const Landing = () => {
   return (
-    <div>
+    <>
       <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
@@ -12,12 +12,14 @@ export const Landing = () => {
             other developers
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">Sign Up</a>
-            <a href="login.html" className="btn btn-light">Login</a>
+            <Link to="/register" className="btn btn-primary">Sign Up</Link>
+            <Link to="/login" className="btn btn-light">Login</Link>
           </div>
         </div>
       </div>
     </section>
-    </div>
+    </>
   )
 }
+
+export default Landing
