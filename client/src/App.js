f0b3a8728b7fc6c2e2d70ad5a47c62e865e6d542
Yogesh <yogeshj591@gmail.com>
2022-01-20 13:55:38 +0530

    Alert component & Action call

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
 import "./App.css";
 // redux
 import { Provider } from 'react-redux';
@@ -19,6 +20,7 @@ const App = () => (
       </Routes>
 
       <section className="container">
+        <Alert />
         <Routes>
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
