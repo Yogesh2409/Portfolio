ca6a673b5057604788b3d98507683d24b83d99c0
Madhusudan23082000 <73375985+Madhusudan23082000@users.noreply.github.com>
2022-01-20 19:47:03 +0530

    8.2 user authenticate

--- config/db.js
+++ config/db.js
@@ -5,9 +5,7 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
     try{
         await mongoose.connect(db);
-        
-       
-
+ 
         console.log('MongoDB Connected...');
     }
     catch(err){
